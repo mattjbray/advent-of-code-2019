dcdb944850ed5a4df879956c85cbb5f7ece69174
Matt Bray <mattjbray@gmail.com>
2019-12-18 22:33:49 +0000

    rust: day 5 part 2

--- rust/src/day_05.rs
+++ rust/src/day_05.rs
@@ -10,6 +10,10 @@ pub fn solve(part: u8, data: Result<String, std::io::Error>) {
             let output = part_1::run(&mut program, 1);
             println!("{:?}", output);
         }
+        2 => {
+            let output = part_1::run(&mut program, 5);
+            println!("{:?}", output);
+        }
         _ => (),
     }
 }
@@ -38,7 +42,7 @@ pub mod part_1 {
         }
     }
 
-    fn op_3<'a, F, M>(program: &mut Vec<i32>, pc: usize, modes: &mut M, f: F) -> Option<usize>
+    fn op_3<'a, F, M>(program: &mut Vec<i32>, pc: usize, modes: &mut M, f: F) -> usize
     where
         F: Fn(&i32, &i32) -> i32,
         M: Iterator<Item = &'a u8>,
@@ -47,9 +51,25 @@ pub mod part_1 {
         let arg_2_mode = next_arg_mode(modes);
         let arg_1 = get(program, pc + 1, arg_1_mode);
         let arg_2 = get(program, pc + 2, arg_2_mode);
-        let &result_addr = program.get(pc + 3).expect("invalid pc");
+        let &result_addr = get(program, pc + 3, Mode::Immediate);
         program[result_addr as usize] = f(arg_1, arg_2);
-        Some(pc + 4)
+        pc + 4
+    }
+
+    fn jump_if<'a, F, M>(program: &mut Vec<i32>, pc: usize, modes: &mut M, f: F) -> usize
+    where
+        F: Fn(&i32) -> bool,
+        M: Iterator<Item = &'a u8>,
+    {
+        let arg1_mode = next_arg_mode(modes);
+        let arg2_mode = next_arg_mode(modes);
+        let arg1 = get(program, pc + 1, arg1_mode);
+        let pc = if f(arg1) {
+            *get(program, pc + 2, arg2_mode) as usize
+        } else {
+            pc + 3
+        };
+        pc
     }
 
     fn step(program: &mut Vec<i32>, pc: usize, input: i32) -> (Option<usize>, Option<i32>) {
@@ -62,18 +82,60 @@ pub mod part_1 {
         };
         let mut modes = modes.as_bytes().iter().rev();
         match opcode.parse::<u8>() {
-            Ok(99) => (None, None),
-            Ok(1) => (op_3(program, pc, &mut modes, |x, y| x + y), None),
-            Ok(2) => (op_3(program, pc, &mut modes, |x, y| x * y), None),
-            Ok(3) => {
+            Ok(99) =>
+            // terminate
+            {
+                (None, None)
+            }
+            Ok(1) =>
+            // add
+            {
+                let pc = op_3(program, pc, &mut modes, |x, y| x + y);
+                (Some(pc), None)
+            }
+            Ok(2) =>
+            // mul
+            {
+                let pc = op_3(program, pc, &mut modes, |x, y| x * y);
+                (Some(pc), None)
+            }
+            Ok(3) =>
+            // store input
+            {
                 let &addr = get(program, pc + 1, Mode::Immediate);
                 program[addr as usize] = input;
                 (Some(pc + 2), None)
             }
-            Ok(4) => {
+            Ok(4) =>
+            // output
+            {
                 let mode = next_arg_mode(&mut modes);
                 let &addr = get(program, pc + 1, mode);
                 (Some(pc + 2), Some(addr))
+            }
+            Ok(5) =>
+            // jump-if-true
+            {
+                let pc = jump_if(program, pc, &mut modes, |&x| x != 0);
+                (Some(pc), None)
+            }
+            Ok(6) =>
+            // jump-if-false
+            {
+                let pc = jump_if(program, pc, &mut modes, |&x| x == 0);
+                (Some(pc), None)
+            }
+            Ok(7) =>
+            // less-than
+            {
+                let pc = op_3(program, pc, &mut modes, |x, y| if x < y { 1 } else { 0 });
+                (Some(pc), None)
+            }
+            Ok(8) =>
+            // equals
+            {
+                let pc = op_3(program, pc, &mut modes, |x, y| if x == y { 1 } else { 0 });
+                (Some(pc), None)
             }
             _ => panic!("invalid instruction"),
         }
@@ -129,4 +191,103 @@ pub mod part_1 {
     }
 }
 
-pub mod part_2 {}
+pub mod part_2 {
+
+    #[cfg(test)]
+    mod tests {
+        use super::super::*;
+
+        #[test]
+        fn test_equal_position_mode() {
+            // outputs 1 if input == 8
+            let program = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8];
+
+            let output = part_1::run(&mut program.clone(), 8);
+            assert_eq!(output, Some(1));
+
+            let output = part_1::run(&mut program.clone(), 9);
+            assert_eq!(output, Some(0));
+        }
+
+        #[test]
+        fn test_less_than_position_mode() {
+            // outputs 1 if input < 8
+            let program = vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8];
+
+            let output = part_1::run(&mut program.clone(), 7);
+            assert_eq!(output, Some(1));
+
+            let output = part_1::run(&mut program.clone(), 8);
+            assert_eq!(output, Some(0));
+        }
+
+        #[test]
+        fn test_equal_immediate_mode() {
+            // outputs 1 if input == 8
+            let program = vec![3, 3, 1108, -1, 8, 3, 4, 3, 99];
+
+            let output = part_1::run(&mut program.clone(), 8);
+            assert_eq!(output, Some(1));
+
+            let output = part_1::run(&mut program.clone(), 9);
+            assert_eq!(output, Some(0));
+        }
+
+        #[test]
+        fn test_less_than_immediate_mode() {
+            // outputs 1 if input < 8
+            let program = vec![3, 3, 1107, -1, 8, 3, 4, 3, 99];
+
+            let output = part_1::run(&mut program.clone(), 7);
+            assert_eq!(output, Some(1));
+
+            let output = part_1::run(&mut program.clone(), 8);
+            assert_eq!(output, Some(0));
+        }
+
+        #[test]
+        fn test_jump_position_mode() {
+            // ouputs 1 if input != 0
+            let program = vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9];
+
+            let output = part_1::run(&mut program.clone(), 10);
+            assert_eq!(output, Some(1));
+
+            let output = part_1::run(&mut program.clone(), 0);
+            assert_eq!(output, Some(0));
+        }
+
+        #[test]
+        fn test_jump_immediate_mode() {
+            // ouputs 1 if input != 0
+            let program = vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1];
+
+            let output = part_1::run(&mut program.clone(), 10);
+            assert_eq!(output, Some(1));
+
+            let output = part_1::run(&mut program.clone(), 0);
+            assert_eq!(output, Some(0));
+        }
+
+        #[test]
+        fn test_large() {
+            // case input:
+            //   < 8 ->  999
+            //  == 8 -> 1000
+            //   > 8 -> 1001
+            let program = vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ];
+
+            let output = part_1::run(&mut program.clone(), 7);
+            assert_eq!(output, Some(999));
+
+            let output = part_1::run(&mut program.clone(), 8);
+            assert_eq!(output, Some(1000));
+            let output = part_1::run(&mut program.clone(), 9);
+            assert_eq!(output, Some(1001));
+        }
+    }
+}
