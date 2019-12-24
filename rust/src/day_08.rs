d3fea502d1b4b5c675f5b256487355631d47c5dd
Matt Bray <mattjbray@gmail.com>
2019-12-24 17:07:10 +0000

    rust: day 8 part 2

--- rust/src/day_08.rs
+++ rust/src/day_08.rs
@@ -3,7 +3,7 @@ pub fn solve(part: u8, data: Result<String, std::io::Error>) {
     let i = part_1::Image::from_str(data.expect("Could not read data file").as_str(), s);
     match part {
         1 => println!("{}", i.checksum().unwrap()),
-        2 => (),
+        2 => println!("{}", i),
         _ => (),
     }
 }
@@ -11,13 +11,17 @@ pub fn solve(part: u8, data: Result<String, std::io::Error>) {
 mod part_1 {
 
     pub struct Size {
-        width: usize,
-        height: usize,
+        pub width: usize,
+        pub height: usize,
     }
 
     impl Size {
         pub fn new(width: usize, height: usize) -> Self {
             Self { width, height }
+        }
+
+        pub fn size(&self) -> usize {
+            self.width * self.height
         }
     }
 
@@ -30,8 +34,8 @@ mod part_1 {
     }
 
     pub struct Image {
-        size: Size,
-        pixels: Vec<u8>,
+        pub size: Size,
+        pub pixels: Vec<u8>,
     }
 
     impl Image {
@@ -64,6 +68,56 @@ mod part_1 {
         fn test_1() {
             let i = Image::from_str("123456789012", Size::new(3, 2));
             assert_eq!(i.pixels[0], 1);
+        }
+    }
+}
+
+mod part_2 {
+    use super::part_1::*;
+
+    impl Image {
+        pub fn render(&self) -> Vec<u8> {
+            let size = self.size.size();
+            let mut result = Vec::new();
+            for i in 0..size {
+                let mut layer = 0;
+                loop {
+                    let px = self.pixels[i + layer*size];
+                    if px == 2 {
+                        layer += 1;
+                    } else {
+                        result.push(px);
+                        break
+                    }
+                }
+            }
+
+            result
+        }
+    }
+
+    impl std::fmt::Display for Image {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let pixels = self.render();
+            let lines = pixels.chunks(self.size.width);
+            for line in lines {
+                for px in line {
+                    write!(f, "{}", if *px == 0 { " " } else { "█" })?;
+                }
+                writeln!(f, "")?;
+            }
+            Ok(())
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_1() {
+            let i = Image::from_str("0222112222120000", Size::new(2, 2));
+            assert_eq!(i.render(), vec![0, 1, 1, 0]);
         }
     }
 }
